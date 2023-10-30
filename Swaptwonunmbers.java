7a211228b33e2bce520c28651539abf027c3191a
nazeema <nehairisbusinessservices@gmail.com>
2023-10-30 08:52:49 +0530

    general programes

---
+++ Swaptwonunmbers.java
@@ -0,0 +1,26 @@
+public class Swaptwonunmbers
+{
+
+        public static void main(String[] args)
+        {
+            int x = 100, y = 200;
+
+
+            System.out.println("x = " + x);
+            System.out.println("y = " + y);
+
+            // Swapping using three
+            // Variables
+            int temp = x;
+            x = y;
+            y = temp;
+
+            System.out.println("After swap");
+            System.out.println("x = " + x);
+            System.out.println("y = " + y);
+        }
+}
+
+
+
+
