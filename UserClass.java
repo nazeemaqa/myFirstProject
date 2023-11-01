a08c47a84b7061695691db8a174f9c51fd80739c
nazeema <nehairisbusinessservices@gmail.com>
2023-11-01 19:40:02 +0530

    Added Another class

---
+++ UserClass.java
@@ -0,0 +1,27 @@
+public class UserClass {
+//attributes
+    String name;
+    int age;
+    int excercisescompleted;
+    // constructor
+    UserClass(String name,int age,int excerciescompleted){
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public int getExcercisescompleted(){
+        return excercisescompleted;
+    }
+
+
+
+
+
+
+
+
+
+}
