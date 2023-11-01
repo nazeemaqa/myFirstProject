92b190b3eb049f99bf9dcf6036d1dc8367baf704
nazeema <nehairisbusinessservices@gmail.com>
2023-11-01 17:36:48 +0530

    Added Exercise Example

---
+++ src/ExcerciseClass.java
@@ -0,0 +1,33 @@
+public class ExcerciseClass {
+    // attributies
+   String  name;
+   int difficultylevel;
+   int durationMinutes;
+   String discription;
+
+// contructor
+   ExcerciseClass (String  name, int difficultylevel,  int durationMinutes, String discription){
+
+    }
+// getters
+
+ public String getName(){
+       return name;
+ }
+ public int getDifficultyLevel(){
+       return difficultylevel;
+ }
+ public int getDurationMinutes(){
+       return durationMinutes;
+
+ }
+ public String getDiscription(){
+    return discription;
+    }
+
+
+
+
+
+
+}
