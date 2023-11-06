2699075641f3466e2bf5266f5cdf651b8ba0d3f7
nazeema <nazeemaqatester123@gmail.com>
2023-11-06 15:57:36 +0530

    Added Overriding Example

---
+++ AbstractionExample.java
@@ -0,0 +1,28 @@
+
+
+
+
+
+    abstract class Mark{
+        // atleast one abstract method
+        public abstract void display(int marks);
+    }
+
+class child extends Mark{
+    public void display(int marks) {
+        System.out.println(marks + "is displayed");
+
+    }}
+public class AbstractionExample {
+
+public static void main (String argsn[]){
+    child c=new child();
+    c.display(45);
+}
+
+
+
+
+
+
+}
